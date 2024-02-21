e8cfa3a4ff514472967b750bd8721341664f4ec4
ByteZO <coder.gny@gmail.com>
2024-02-21 19:46:11 +0530

    #3 Button update

--- GreyBoard/src/Components/Buttom/Button.jsx
+++ GreyBoard/src/Components/Buttom/Button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Button = ({children}) => {
+const Button = ({ children }) => {
   return (
     <>
-      <div className="  h-14 w-14 border-2   rounded-md flex justify-center items-center  ">
+      <div className="  	 shadow-lg shadow-white-800/50 h-14 w-14 border-2  bg-gray-900 text-white   rounded-md flex justify-center items-center r">
+
         {children}
       </div>
     </>
   );
-};   
+};
 
 export default Button;
--- GreyBoard/src/Components/ToolBar/ToolBar.jsx
+++ GreyBoard/src/Components/ToolBar/ToolBar.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Button from "../Buttom/Button";
 
-
-const ToolBar = ({colorChanger, markerHandle}) => {
+const ToolBar = ({ colorChanger, markerHandle }) => {
   return (
     <>
-      <div className="  absolute bottom-0 h-20   w-full   flex items-center  justify-center  border-2 ">
-        <div className=" flex   items-center   justify-around h-16  w-1/2  rounded-md border-2 ">
+      <section className="  absolute bottom-0 h-20 w-full   flex items-center justify-between  px-7 border-2  ">
+        <Button>
+          <i class="ri-camera-3-line"></i>
+        </Button>
+
+        <section className=" flex   items-center bg-gray-400  justify-around h-16  w-1/2  rounded-md border-2 ">
           <Button>
             <i className="ri-pencil-line"></i>
           </Button>
           <Button>
             <i className="ri-arrow-right-up-line"></i>
+          </Button>
+          <Button>
+          <i class="ri-text-snippet"></i>
           </Button>
           <Button>
             <i className="ri-rectangle-line"></i>
@@ -20,12 +26,19 @@ const ToolBar = ({colorChanger, markerHandle}) => {
             <i className="ri-eraser-line"></i>
           </Button>
           <Button>
-            <i className="ri-color-filter-line">
-              <input  type="color" value={colorChanger} onChange={markerHandle} />
-            </i>
+            <i className="ri-color-filter-line"></i>
           </Button>
-        </div>
-      </div>
+        </section>
+
+        <section className=" flex space-x-5" >
+        <button className="border-2   bg-gray-900 text-white  rounded-md flex justify-center items-center p-3">
+            SignUp
+          </button>
+          <button className="border-2   bg-gray-900 text-white  rounded-md flex justify-center items-center p-3">
+            LogIn
+          </button>
+        </section>
+      </section>
     </>
   );
 };
